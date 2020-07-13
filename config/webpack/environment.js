4f7b90537c6e43403271f4b801bf16f2ff341693
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-07-13 20:27:18 +0900

    AA 로 인한 디펜던시 조정

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,10 @@
 const { environment } = require('@rails/webpacker')
-const jquery = require('./plugins/jquery')
 const webpack = require('webpack')
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    Popper: ['popper.js', 'default']
+    "$":"jquery",
+    "jQuery":"jquery",
+    "Popper": ['popper.js', 'default']
 }))
 
-environment.plugins.prepend('jquery', jquery)
 module.exports = environment
