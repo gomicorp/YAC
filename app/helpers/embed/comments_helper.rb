837da534dde87f32e099d51b8c9ae8f3f24a3ce1
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-07-15 15:22:05 +0900

    bugfix: HelperComponent 클래스 로딩 실패 문제

--- app/helpers/embed/comments_helper.rb
+++ app/helpers/embed/comments_helper.rb
@@ -1,5 +1,22 @@
 require_relative '../../../config/initializers/helper_component'
 
+class HelperComponent
+  def initialize(view)
+    @view = view
+  end
+
+  def render(options = {}, locals = {}, &block)
+    @view.render(options, locals, &block)
+  end
+
+  module Factory
+    def self.declare_component_interface_of(component_classname)
+      Object.const_get(component_classname).new(self)
+    end
+  end
+end
+
+
 module Embed
   module CommentsHelper
     # include HelperComponent::Factory
