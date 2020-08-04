62448b7be93dd4fef92282b8caa8447d1ddf2027
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-08-04 20:47:50 +0900

    Add landing privacy & terms page mockup

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,22 @@
 class LandingController < ApplicationController
   layout 'landing'
+  before_action :layout_false, only: %i[main terms privacy]
 
   def main
+    #
+  end
+
+  def terms
+    #
+  end
+
+  def privacy
+    #
+  end
+
+  private
+
+  def layout_false
     render layout: false
   end
 end
